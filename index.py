4ecdba6c21b7f0e45d1c0b2d64ae4f57b9e671ff
Colin Waddell <mrcolin@gmail.com>
2017-09-23 13:15:36 +0100

    markdown + templating

--- index.py
+++ index.py
@@ -1,7 +1,24 @@
-from flask import Flask
+import markdown
+from flask import Flask, Markup, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    content = """
+Chapter
+=======
 
+Section
+-------
+
+* Item 1
+* Item 2
+"""
+    content = Markup(markdown.markdown(content))
+    return render_template('index.html', **locals())
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, use_reloader=True, host='0.0.0.0')
