12bcb78773859e975e323d4f706b79f26a185ebd
Colin Waddell <mrcolin@gmail.com>
2017-09-23 13:53:35 +0100

    grunting project

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import markdown
 from flask import Flask, Markup, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 def index():
@@ -21,4 +23,5 @@ Section
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
     app.run(debug=True, use_reloader=True, host='0.0.0.0')
